58dfe3c0451414a0b879e0c746d33e693e485d62
Glenn Tweedie <glenn.tweedie@agworld.com.au>
2011-03-30 17:47:05 +0800

    reset the STDERR and STDOUT redirection

--- lib/deadweight/hijack.rb
+++ lib/deadweight/hijack.rb
@@ -1,12 +1,28 @@
 class Deadweight
   module Hijack
     def self.redirect_output(log_file_prefix)
-      original_stdout, original_stderr = STDOUT.clone, STDERR.clone
+      @original_stdout, @original_stderr = STDOUT.clone, STDERR.clone
+      STDOUT.reopen( File.open( "#{log_file_prefix}stdout.log", 'w') )
+      STDERR.reopen( File.open( "#{log_file_prefix}stderr.log", 'w') )
 
-      STDOUT.reopen(File.open("#{log_file_prefix}stdout.log", 'w'))
-      STDERR.reopen(File.open("#{log_file_prefix}stderr.log", 'w'))
-
-      [original_stdout, original_stderr]
+      [@original_stdout, @original_stderr]
+    end
+    
+    def self.reset_output
+      if @original_stderr
+        @original_stderr.flush
+        @hijacked_stderr = STDERR.clone
+        STDERR.reopen( @original_stderr )
+        @hijacked_stderr.close
+        warn File.read( @hijacked_stderr.path )
+      end
+      if @original_stdout
+        @original_stdout.flush
+        @hijacked_stdout = STDOUT.clone
+        STDOUT.reopen( @original_stdout )
+        @hijacked_stdout.close
+        puts File.read( @hijacked_stdout.path )
+      end
     end
   end
 end
--- lib/deadweight/hijack/rails.rb
+++ lib/deadweight/hijack/rails.rb
@@ -1,4 +1,5 @@
 if ENV['DEADWEIGHT'] == 'true'
+  puts "deadweight enabled"
   require 'deadweight'
   require 'deadweight/hijack'
   require 'deadweight/rack/capturing_middleware'
@@ -12,12 +13,13 @@ if ENV['DEADWEIGHT'] == 'true'
           original_stdout, original_stderr = Deadweight::Hijack.redirect_output(root + 'log/test_')
           dw = Deadweight.new
           dw.root        = root + 'public'
-          dw.stylesheets = Dir.chdir(dw.root) { Dir.glob("stylesheets/*.css") }
+          dw.stylesheets = Dir.chdir(dw.root) { Dir.glob("stylesheets/screen.css") } # can use globs here
           dw.log_file    = original_stderr
           dw.reset!
           at_exit do
             dw.report
             dw.dump(original_stdout)
+            Deadweight::Hijack.reset_output
           end
     	    Rails.configuration.middleware.insert_after( 'ActionController::Failsafe', Deadweight::Rack::CapturingMiddleware, dw ) 
         else
