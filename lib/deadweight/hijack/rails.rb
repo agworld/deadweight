242f2d115edfdd229f5f4786e31cbbfc4f8f168a
Glenn Tweedie <glenn.tweedie@agworld.com.au>
2011-03-30 11:10:58 +0800

    add support for Rails v2.3

--- lib/deadweight/hijack/rails.rb
+++ lib/deadweight/hijack/rails.rb
@@ -5,30 +5,47 @@ if ENV['DEADWEIGHT'] == 'true'
 
   class Deadweight
     module Hijack
-      module Rails
-        class Railtie < ::Rails::Railtie
-          initializer "deadweight.hijack" do |app|
-            root = ::Rails.root
+	    if defined?( Rails ) 
+        if Rails::VERSION::MAJOR == 2
+          # Set up a deadweight instance
+          root = ::Rails.root
+          original_stdout, original_stderr = Deadweight::Hijack.redirect_output(root + 'log/test_')
+          dw = Deadweight.new
+          dw.root        = root + 'public'
+          dw.stylesheets = Dir.chdir(dw.root) { Dir.glob("stylesheets/*.css") }
+          dw.log_file    = original_stderr
+          dw.reset!
+          at_exit do
+            dw.report
+            dw.dump(original_stdout)
+          end
+    	    Rails.configuration.middleware.insert_after( 'ActionController::Failsafe', Deadweight::Rack::CapturingMiddleware, dw ) 
+        else
+          module Rails
+            class Railtie < ::Rails::Railtie
+              railtie_name :deadweight_hijack
+            
+              initializer "deadweight.hijack" do |app|
+                # Set up a deadweight instance
+                root = ::Rails.root
+                original_stdout, original_stderr = Deadweight::Hijack.redirect_output(root + 'log/test_')
+                dw = Deadweight.new
+                dw.root        = root + 'public'
+                dw.stylesheets = Dir.chdir(dw.root) { Dir.glob("stylesheets/*.css") }
+                dw.log_file    = original_stderr
+                dw.reset!
+                at_exit do
+                  dw.report
+                  dw.dump(original_stdout)
+                end
 
-            original_stdout, original_stderr = Deadweight::Hijack.redirect_output(root + 'log/test_')
+                app.middleware.insert(0, Deadweight::Rack::CapturingMiddleware, dw )
 
-            dw = Deadweight.new
-
-            dw.root        = root + 'public'
-            dw.stylesheets = Dir.chdir(dw.root) { Dir.glob("stylesheets/*.css") }
-            dw.log_file    = original_stderr
-
-            dw.reset!
-
-            at_exit do
-              dw.report
-              dw.dump(original_stdout)
-            end
-
-            app.middleware.insert(0, Deadweight::Rack::CapturingMiddleware, dw)
+              end
+          	end
           end
         end
-      end
+    	end
     end
   end
 end
